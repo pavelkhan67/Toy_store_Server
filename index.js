f6c43f22110e16e59f4152b89b9dd333e8ab9b88
Pavel Khan <pvelkhantopu@gmail.com>
2023-05-17 22:24:28 +0600

    Initial server setup complete

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Toy Store is running')
+})
+
+app.listen(port, () => {
+    console.log(`Toy Store is running on port: ${port}`);
+})

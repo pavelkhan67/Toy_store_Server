c255f74f9273d938203a93371ee3fe79418a9dfd
Pavel Khan <pvelkhantopu@gmail.com>
2023-05-18 19:03:28 +0600

    Show user added toy based on email

--- index.js
+++ index.js
@@ -58,6 +58,16 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/mytoys/:email", async (req, res) => {
+            console.log(req.params.id);
+            const jobs = await toyCollection
+              .find({
+                email: req.params.email,
+              })
+              .toArray();
+            res.send(jobs);
+          });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
